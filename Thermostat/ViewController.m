2a89eb0a0fe305c030babd40373179fdbef5a63d
colbygatte <colbygatte@gmail.com>
2016-10-03 11:57:59 -0500

    Initial Commit

---
+++ Thermostat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Thermostat
+//
+//  Created by Colby Gatte on 10/3/16.
+//  Copyright © 2016 Colby Gatte. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
